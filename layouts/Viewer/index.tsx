1af5c7f72f8f389f72b9acc65375130d6e08e612
kimyerimweb <kimyerimweb@gmail.com>
2022-04-27 15:15:22 +0900

    feat: 뷰어에 이미지 보이게 만듦

--- layouts/Viewer/index.tsx
+++ layouts/Viewer/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 import { RootState } from '@store/configureStore';
@@ -7,23 +7,49 @@ import { replaceValue, toggleView } from '@reducer/viewSlice';
 export default function Viewer() {
   const { view, value } = useSelector((state: RootState) => state.view);
   const dispatch = useDispatch();
+  const [imageUrl, setImageUrl] = useState<string | ArrayBuffer | null | undefined>();
+
+  useEffect(() => {
+    handleShowImage();
+
+    return () => {
+      handleShowImage();
+    };
+  });
 
   const handleCloseViewer = useCallback(() => {
     dispatch(toggleView(false));
     dispatch(replaceValue(null));
   }, [dispatch]);
 
+  const handleShowImage = useCallback(() => {
+    if (!value || 'url' in value) {
+      return false;
+    }
+    const reader = new FileReader();
+    reader.onload = () => setImageUrl(reader.result);
+    reader.readAsDataURL(value.image);
+  }, [value]);
+
   return (
     <div style={{ backgroundColor: '#fff' }}>
       {view && (
         <div>
-          <div>
-            <header>{value && 'url' in value ? value.name : value?.image.name}</header>
+          <header>
+            <h1>{value && 'url' in value ? value.name : value?.image.name}</h1>
             <button type="button" onClick={handleCloseViewer}>
               닫기
             </button>
-          </div>
-          {value && 'url' in value ? <iframe src={value.url} frameBorder="0"></iframe> : null}
+          </header>
+          <main>
+            {value && 'url' in value ? (
+              <iframe src={value.url} frameBorder="0"></iframe>
+            ) : (
+              <div>
+                <img src={typeof imageUrl === 'string' ? imageUrl : undefined} alt="" />
+              </div>
+            )}
+          </main>
         </div>
       )}
     </div>
